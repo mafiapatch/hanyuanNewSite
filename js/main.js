0192841beb97e7cea465ead062a1e576bb34e853
Mafia hsu <g8071107@gmail.com>
2021-02-06 14:17:44 +0800

    backup

--- js/main.js
+++ js/main.js
@@ -133,6 +133,37 @@
 	};
 	carousel();
 
+
+	//room picture slider.
+document.addEventListener( 'DOMContentLoaded', function () {
+	var secondarySlider = new Splide( '#secondary-slider', {
+		fixedWidth  : 100,
+		height      : 60,
+		gap         : 10,
+		cover       : true,
+		isNavigation: true,
+		focus       : 'center',
+		breakpoints : {
+			'600': {
+				fixedWidth: 66,
+				height    : 40,
+			}
+		},
+	} ).mount();
+	
+	var primarySlider = new Splide( '#primary-slider', {
+		type       : 'fade',
+		heightRatio: 0.5,
+		pagination : false,
+		arrows     : false,
+		cover      : true,
+	} ); // do not call mount() here.
+	
+	primarySlider.sync( secondarySlider ).mount();
+} );
+
+
+
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
 		// 	 timer;
